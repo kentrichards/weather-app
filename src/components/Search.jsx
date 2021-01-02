4d299ffb64b9484fdd25f3c39c45be1d2e03c13a
Kent Richards <kent-richards@outlook.com>
2021-01-01 20:28:30 -0400

    change to modal pt1

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,66 +1,13 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useCallback } from 'react'
-import debounce from 'lodash.debounce'
+import React, { useState } from 'react'
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [queryResponse, setQueryResponse] = useState('')
-
-  const fetchPlace = async query => {
-    await fetch(
-      `${process.env.GEOCODE_URL}${query}.json?types=place&limit=4&access_token=${process.env.GEOCODE_KEY}`
-    )
-      .then(response => response.json())
-      .then(result => setQueryResponse(result))
-  }
-
-  const delayedSearch = useCallback(
-    debounce(query => fetchPlace(query), 300),
-    []
-  )
-
-  useEffect(() => {
-    if (searchQuery.length !== 0) {
-      delayedSearch(searchQuery)
-    }
-
-    // Cancel pending function calls when the component unmounts
-    return delayedSearch.cancel
-  }, [searchQuery, delayedSearch])
-
-  const handleChange = event => {
-    setSearchQuery(event.target.value)
-  }
-
-  let searchResults = ''
-  if (queryResponse) {
-    if (!queryResponse.features || queryResponse.features.length === 0) {
-      searchResults = <li className="no-results">No results found</li>
-    } else if (queryResponse.features) {
-      searchResults = queryResponse.features.map(place => (
-        <li className="search-result" key={place.id}>
-          {place.place_name}
-        </li>
-      ))
-    }
-  }
+  const [showModal, setShowModal] = useState(false)
 
   return (
-    <div className="relative">
-      <input
-        type="text"
-        name="location"
-        id="location"
-        className="input"
-        value={searchQuery}
-        onChange={e => handleChange(e)}
-        placeholder="Toronto, Ontario"
-      />
-      {searchQuery && (
-        <ul className="absolute w-full mt-2 bg-white overflow-hidden rounded-md shadow border border-gray-300">
-          {searchResults}
-        </ul>
-      )}
+    <div>
+      <button type="button" className="input-btn">
+        Toronto, Ontario, Canada
+      </button>
     </div>
   )
 }
